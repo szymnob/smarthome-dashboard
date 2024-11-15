dacd0d96c42b9130f8718b107744db2e5d26b295
szymnob <szymon6457@gmail.com>
2024-11-15 14:40:28 +0100

    k

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
       colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
