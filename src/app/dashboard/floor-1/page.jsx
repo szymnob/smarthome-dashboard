deeb2536b4c808ae004eb63d26441cbee4d13177
szz0b <szymon6457@gmail.com>
2024-11-16 22:16:01 +0100

    added exemplary floor page

---
+++ src/app/dashboard/floor-1/page.jsx
@@ -0,0 +1,11 @@
+import Header from "@/app/ui/dashboard/RectHeader";
+
+export default function Page() {
+    return(
+        <>
+            <Header>
+                <h1 className="text-2xl text-bold">Floor 1</h1>
+            </Header>
+        </>
+    )
+}
