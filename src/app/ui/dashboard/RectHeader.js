413f5796a1e5431e7521623b25785ece09f45224
szymnob <szymon6457@gmail.com>
2024-11-17 23:53:20 +0100

    .

--- src/app/ui/dashboard/RectHeader.js
+++ src/app/ui/dashboard/RectHeader.js
@@ -1,7 +1,7 @@
 
 export default function Header({ children }) {
     return (
-        <div className="flex flex-row w-full justify-between bg-gray-300 p-5 rounded-lg">
+        <div className="flex flex-row w-full justify-between bg-gray-300 p-7 rounded-lg">
             {children}
         </div>
     );
