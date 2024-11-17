7c2f5be44ec6faa1a7a632625c1c2787501471cc
szymnob <szymon6457@gmail.com>
2024-11-17 23:51:23 +0100

    added new functions

--- src/app/dashboard/dataService.js
+++ src/app/dashboard/dataService.js
@@ -2,3 +2,41 @@
 export function getFloorsNumbers(data){
     return Object.keys(data.home.floors).map(floorId => floorId);
 }
+
+export default function getRooms(data, floorId){
+    return data.home.floors[floorId].rooms;
+}
+
+export function getRoomName(data, floorId, roomId){
+    return data.home.floors[floorId].rooms[roomId].name;
+}
+
+export function getDevicesIdInRoom(data, floorId, roomId){
+    return data.home.floors[floorId].rooms[roomId].devices;
+}
+
+export function getDeviceNameById(data, deviceId) {
+    const device = data.devices[deviceId];
+
+    if (device) {
+        return device.name;
+    }
+    return null;
+}
+
+export function getDeviceStateById(data, deviceId) {
+    const deviceState = data.devices[deviceId].state;
+
+    if (deviceState) {
+        return deviceState;
+    }
+    return null;
+}
+
+export function getDeviceName(data, floorId, roomId, deviceId){
+    return data.home.floors[floorId].rooms[roomId].devices[deviceId].name;
+}
+
+export function getRoomsIdOnFloor(data, floorId){
+    return Object.keys(data.home.floors[floorId].rooms).map(roomId => roomId);
+}
