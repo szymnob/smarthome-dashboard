adefe2456354765813a1d9dfc415bd7d60fa6f82
szz0b <szymon6457@gmail.com>
2024-11-17 17:08:27 +0100

    added data provider

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,12 +1,16 @@
+
 import Navigation from '@/app/ui/dashboard/Navigation';
+import {DataProvider} from './dataContext.js';
 
 export default function Layout({ children, }) {
     return (
+        <DataProvider>
         <div className="flex flex-row h-screen md:overflow-hidden">
             <div className=" flex-none">
                 <Navigation />
             </div>
             <div className="flex-grow md:overflow-y-auto p-4">{children}</div>
         </div>
+        </DataProvider>
     );
 }
