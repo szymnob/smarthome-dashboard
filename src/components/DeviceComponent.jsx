1a245f8e38f17932417d0af30b45a1032950b758
szymnob <szymon6457@gmail.com>
2024-11-18 01:32:10 +0100

    added device type state

--- src/components/DeviceComponent.jsx
+++ src/components/DeviceComponent.jsx
@@ -1,7 +1,7 @@
 
 import {use, useContext, useEffect, useState} from 'react';
 import DataContext from '@/app/dashboard/dataContext';
-import {getDeviceNameById, getDeviceStateById} from "@/app/dashboard/dataService";
+import {getDeviceNameById, getDeviceStateById, getDeviceTypeById} from "@/app/dashboard/dataService";
 import Switch from "@/components/Switch";
 import clsx from "clsx";
 
@@ -12,14 +12,18 @@ export default function DeviceComponent({deviceId}) {
     const [deviceName, setDeviceName] = useState("")
     const [deviceState, setDeviceState] = useState("")
     const [status, setStatus] = useState(null);
+    const [deviceType, setDeviceType] = useState("");
 
     useEffect(() => {
         if (data) {
             const name = getDeviceNameById(data, deviceId);
             const state = getDeviceStateById(data, deviceId);
+            const type = getDeviceTypeById(data, deviceId);
 
             setDeviceName(name);
             setDeviceState(state);
+            setDeviceType(type);
+
             if (state && state.status) {
                 setStatus(state.status);
             }
@@ -40,7 +44,8 @@ export default function DeviceComponent({deviceId}) {
         <div className={clsx(
             "flex flex-col items-center justify-center bg-white rounded-xl shadow-md w-48 m-5",
             {
-                "bg-violet-600": status === "ON",
+                "bg-gradient-radial from-violet-500 to-violet-600": status === "ON",
+
             }
         )}>
 
@@ -54,8 +59,8 @@ export default function DeviceComponent({deviceId}) {
                 </div>
 
                 <div className="flex flex-row justify-between">
-                    <img src="https://via.placeholder.com/120" alt="Device" className="w-10 h-10 rounded-full"/>
-                    <img src="https://via.placeholder.com/80" alt="Device" className="w-10 h-10 rounded-full"/>
+                    <img src={`/icons/${deviceType}.svg`} alt={deviceName} className="w-12 h-12"/>
+                    <img src="/icons/settings.svg" alt="Device" className="w-10 h-10"/>
                 </div>
                 <div>
 
