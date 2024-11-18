548514b3c8553697bd7f8fa33da9768b2daa4032
szymnob <szymon6457@gmail.com>
2024-11-18 22:56:05 +0100

    added animation (yet not working)

--- src/components/control-overlays/ModalWindow.jsx
+++ src/components/control-overlays/ModalWindow.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 
 
 export default function ModalWindow({isVisible, onClose, title, children}) {
@@ -6,11 +7,19 @@ export default function ModalWindow({isVisible, onClose, title, children}) {
 
     return(
         <>
-            <div className="fixed inset-0 bg-black backdrop-blur-sm bg-opacity-50 flex items-center justify-center z-50"
-                onClick={onClose}
-                >
-                <div className="flex-col w-60 h-60 overflow-auto bg-neutral-50 shadow-md border-neutral-300 rounded-lg border-2"
-                    onClick={(e)=> e.stopPropagation()}>
+            <div className={clsx("fixed inset-0 bg-black backdrop-blur-sm bg-opacity-50 transition-opacity duration-600 flex items-center justify-center z-50",
+                {
+                    "opacity-0 pointer-events-none": !isVisible,
+                    "opacity-100": isVisible,
+                })}
+                 onClick={onClose}
+            >
+                <div className={clsx("flex-col w-60 h-60 overflow-auto transform transition-transform duration-300 bg-neutral-50 shadow-md border-neutral-300 rounded-lg border-2",
+                    {
+                        "scale-100": isVisible,
+                        "scale-90": !isVisible,
+                    })}
+                     onClick={(e)=> e.stopPropagation()}>
 
                     <div className="flex flex-row justify-between items-center p-2 border-b-2">
                         <h1 className="text-base text-bold text-center">{title}</h1>
