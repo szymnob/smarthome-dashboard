b4edcfd852b902fdf0ba3e9335121cc7f69863a2
szymnob <szymon6457@gmail.com>
2024-11-19 21:34:19 +0100

    changed error and add handling for floor name error

--- src/components/control-overlays/AddNewFloor.jsx
+++ src/components/control-overlays/AddNewFloor.jsx
@@ -23,40 +23,52 @@ export default function AddNewFloor({onClose}) {
     const[floorNumber, setFloorNumber] = useState('');
     const[floorName, setFloorName] = useState('');
 
-    const[error, setError] = useState("");
+    const[errorNumber, setErrorNumber] = useState("");
+    const[errorName, setErrorName] = useState("");
 
     const handleSubmit = (e) =>{
         e.preventDefault()
-        console.log("Floor number: ", floorNumber);
-        console.log("Floor name: ", floorName);
 
-        addFloor(data,setData, floorNumber, floorName);
-        console.log(data);
+        if(floorName.length < 4){
+            setErrorName("Floor name too short");
+        }
+
+        else if (!errorName && !errorNumber) {
+            console.log("sdfsd", floorName, floorNumber);
+            addFloor(data, setData, floorNumber, floorName);
+            onClose();
+        }
     }
 
     const handleFloorNumberChange = (e) => {
         const value = e.target.value;
+        setFloorNumber(value);
+
         if(existingFloors.includes(value)){
-            setError("Floor already exists");
+            setErrorNumber("Floor already exists");
         }else if (value > 6 || value < -3){
-            setError("Floor number out of range");
+            setErrorNumber("Floor number out of range");
 
         }
         else{
-            setError("");
+            setErrorNumber("");
             setFloorNumber(value);
         }
     }
 
+
+
     const divInputStyle = "flex flex-row justify-between space-x-5";
 
     return (
-        <>
             <div className="m-5">
                 <form onSubmit={handleSubmit} className="space-y-5">
 
-                    <Input id="floorNumber" label="Floor number:" type="number" value={floorNumber} onChange={handleFloorNumberChange}/>
-                    <Input id="floorName" label="Floor name:" type="text" value={floorName} onChange={(e) => setFloorName(e.target.value)}/>
+                    <Input id="floorNumber" label="Floor number:" type="number" error={errorNumber} value={floorNumber} onChange={handleFloorNumberChange}/>
+                    <Input id="floorName" label="Floor name:" type="text"  error={errorName} value={floorName} onChange={(e) => setFloorName(e.target.value)}/>
+
+                    {(errorNumber || errorName) && <div
+                        className="w-full text-center bg-red-400 shadow-md shadow-red-300 rounded-lg leading-loose">{errorNumber || errorName}</div>}
 
                     <div className="flex flex-row justify-between space-x-5 p-2">
                         <button className="flex-1 text-gray-700 bg-gray-100 leading-loose rounded-md border-2 border-gray-300 hover:bg-gray-200 active:bg-gray-300 shadow-sm" onClick={onClose}>Cancel</button>
@@ -67,7 +79,6 @@ export default function AddNewFloor({onClose}) {
                 </form>
 
             </div>
-        </>
     )
 }
 
--- src/components/control-overlays/Input.jsx
+++ src/components/control-overlays/Input.jsx
@@ -1,21 +1,24 @@
+import clsx from "clsx";
 
 
 export default function Input({  id, label, type = "text", value, onChange, error}) {
 
 
     return(
-        <>
             <div className="flex flex-col space-y-4">
-                <div className="flex flex-row justify-between space-x-4">
+                <div className="flex flex-row items-center justify-between space-x-4">
                     <label htmlFor={id}>
                         {label}
                     </label>
-                    <input value={value} className="rounded border-2 border-violet-100 focus:outline-none focus:border-2 focus:border-violet-700 focus:shadow focus:shadow-violet-500" id={id} type={type} onChange={onChange}/>
+                    <input value={value} id={id} type={type} onChange={onChange} className={clsx("rounded-lg border-2 p-1 border-violet-100 focus:outline-none focus:border-2 focus:border-violet-700 focus:shadow focus:shadow-violet-500",
+                        {
+                            "border-red-400 focus:border-red-400 focus:shadow-red-300": error,
+                        })}
+                    />
                 </div>
 
-                {error && <div
-                    className="w-full text-center bg-red-400 shadow-md shadow-red-300 rounded-lg leading-loose">{error}</div>}
+                {/*{error && <div*/}
+                {/*    className="w-full text-center bg-red-400 shadow-md shadow-red-300 rounded-lg leading-loose">{error}</div>}*/}
             </div>
-        </>
     )
 }
