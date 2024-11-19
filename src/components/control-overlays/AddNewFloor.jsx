ec6113c30c1615978909f30aa091b71f11c17429
szymnob <szymon6457@gmail.com>
2024-11-19 01:13:22 +0100

    form to add new floor

--- src/components/control-overlays/AddNewFloor.jsx
+++ src/components/control-overlays/AddNewFloor.jsx
@@ -1,21 +1,91 @@
 
-
 import ModalWindow from "@/components/control-overlays/ModalWindow";
+import {useContext, useEffect, useState} from "react";
+import dataContext from "@/app/dashboard/dataContext";
+import {addFloor, getFloorsNumbers, getRoomsIdOnFloor} from "@/app/dashboard/dataService";
+import DataContext from "@/app/dashboard/dataContext";
+import Input from "@/components/control-overlays/Input";
 
-
-export default function AddNewFloor({isVisible, onClose}) {
-
+export default function AddNewFloor({onClose}) {
     const modalTitle = "Add new floor";
 
-    if (!isVisible) {
-        return null;
+    const { data } = useContext(DataContext);
+    const {setData} = useContext(DataContext);
+
+    const[existingFloors, setExistingFloors] = useState([]);
+
+    useEffect(() => {
+        if (data) {
+            setExistingFloors(getFloorsNumbers(data));
+        }
+    }, [data]);
+
+    const[floorNumber, setFloorNumber] = useState('');
+    const[floorName, setFloorName] = useState('');
+
+    const[error, setError] = useState("");
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log("Floor number: ", floorNumber);
+        console.log("Floor name: ", floorName);
+
+        addFloor(data,setData, floorNumber, floorName);
+        console.log(data);
     }
-    console.log("dupa")
+
+    const handleFloorNumberChange = (e) => {
+        const value = e.target.value;
+        if(existingFloors.includes(value)){
+            setError("Floor already exists");
+        }else if (value > 6 || value < -3){
+            setError("Floor number out of range");
+
+        }
+        else{
+            setError("");
+            setFloorNumber(value);
+        }
+    }
+
+    const divInputStyle = "flex flex-row justify-between space-x-5";
+
     return (
         <>
-        <ModalWindow isVisible={isVisible} onClose={onClose} title={modalTitle}>
-            <h1>USUSUFUDUFSFU</h1>
-        </ModalWindow>
+            <div className="m-5">
+                <form onSubmit={handleSubmit} className="space-y-5">
+
+                    <Input id="floorNumber" label="Floor number:" type="number" value={floorNumber} onChange={handleFloorNumberChange}/>
+                    <Input id="floorName" label="Floor name:" type="text" value={floorName} onChange={(e) => setFloorName(e.target.value)}/>
+
+                    <div className="flex flex-row justify-between space-x-5 p-2">
+                        <button className="flex-1 text-gray-700 bg-gray-100 leading-loose rounded-md border-2 border-gray-300 hover:bg-gray-200 active:bg-gray-300 shadow-sm" onClick={onClose}>Cancel</button>
+
+                        <button className="flex-1 text-white bg-violet-500 leading-loose rounded-md border-2 border-violet-700 active:bg-violet-600 shadow-md shadow-violet-500 active:shadow-violet-500 active:shadow-lg hover:bg-violet-400 "
+                            type="submit">Add</button>
+                    </div>
+                </form>
+
+            </div>
         </>
     )
 }
+
+
+// import ModalWindow from "@/components/control-overlays/ModalWindow";
+//
+// export default function AddNewFloor({isVisible, onClose}) {
+//
+//     const modalTitle = "Add new floor";
+//
+//     if (!isVisible) {
+//         return null;
+//     }
+//     return (
+//         <>
+//         <ModalWindow isVisible={isVisible} onClose={onClose} title={modalTitle}>
+//             <h1>USUSUFUDUFSFU</h1>
+//         </ModalWindow>
+//         </>
+//     )
+// }
