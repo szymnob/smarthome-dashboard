2c8b646f9f11b20b8260e7b036b2062f96a073cf
Dinolek <git@dinolek.me>
2025-01-11 14:37:36 +0100

    Show edit button when appropriate
    Signed-off-by: Dinolek <git@dinolek.me>

--- src/components/ui/assets/RectHeader.js
+++ src/components/ui/assets/RectHeader.js
@@ -16,17 +16,23 @@
 import PropTypes from "prop-types";
 
 export default function Header({ label, isEditing, onEditToggle }) {
-    return (
-        <div className="flex flex-row justify-between bg-neutral-50 shadow-md border-custom p-7 rounded-lg m-5">
-            <h1 className="text-4xl font-bold">{label}</h1>
-
-            {/* Toggle Editing Button */}
+    let editButton = null;
+    if (onEditToggle) {
+        editButton = (
             <button
                 className="px-4 py-2 bg-blue-500 text-white rounded"
                 onClick={onEditToggle}
             >
                 {isEditing ? "Disable Editing" : "Enable Editing"}
             </button>
+        );
+    }
+
+    return (
+        <div className="flex flex-row justify-between bg-neutral-50 shadow-md border-custom p-7 rounded-lg m-5">
+            <h1 className="text-4xl font-bold">{label}</h1>
+
+            {editButton}
         </div>
     );
 }
