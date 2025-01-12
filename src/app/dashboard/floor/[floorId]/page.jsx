4b70b07955cb8370a5e479f8e07c8bfba86a3906
rbernacki <rav.bernacki@gmail.com>
2025-01-12 20:23:43 +0100

    Resolution fix

--- src/app/dashboard/floor/[floorId]/page.jsx
+++ src/app/dashboard/floor/[floorId]/page.jsx
@@ -107,7 +107,7 @@ export default function Page({ params }) {
 
             <div className="flex flex-wrap justify-center m-2">
                 {roomsId.map((roomId) => (
-                    <div key={roomId} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div key={roomId} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-2">
                         <RoomComponent
                             floorId={floorId}
                             roomId={roomId}
